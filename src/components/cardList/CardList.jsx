354155c314bd8b5609dd2bb954e51faf82c189b5
this-is-yogesh <ayogesh1214@gmail.com>
2023-09-19 14:13:37 +0530

    fifth commit

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -6,7 +6,7 @@ import Card from "../card/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `${process.env.NEXTAUTH_URL}/api/posts?page=${page}&cat=${cat || ""}`,
+    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }
